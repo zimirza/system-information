3aa2e9eac8f3da1765ac3de2a608bee69a9941a9
Zishan Mirza <zishan.mirza@h-d-gmbh.de>
2024-05-16 19:31:13 +0200

    use libc to get uname from utsname

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,68 @@
+use std::arch::asm;
 use std::collections::HashMap;
 use std::env;
+use std::ffi::CStr;
+
+use libc::c_char;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct Utsname {
+    pub sysname: [c_char; 65],
+    pub nodename: [c_char; 65],
+    pub release: [c_char; 65],
+    pub version: [c_char; 65],
+    pub machine: [c_char; 65],
+    pub domainname: [c_char; 65],
+}
+
+impl Utsname {
+    fn new() -> Self {
+        Self {
+            sysname: [0; 65],
+            nodename: [0; 65],
+            release: [0; 65],
+            version: [0; 65],
+            machine: [0; 65],
+            domainname: [0; 65],
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Uname {
+    pub sysname: String,
+    pub nodename: String,
+    pub release: String,
+    pub version: String,
+    pub machine: String,
+    pub domainname: String,
+}
+
+impl From<Utsname> for Uname {
+    fn from(utsname: Utsname) -> Self {
+        Self {
+            sysname: { unsafe { CStr::from_ptr(utsname.sysname.as_ptr()) } }
+                .to_string_lossy()
+                .to_string(),
+            nodename: { unsafe { CStr::from_ptr(utsname.nodename.as_ptr()) } }
+                .to_string_lossy()
+                .to_string(),
+            release: { unsafe { CStr::from_ptr(utsname.release.as_ptr()) } }
+                .to_string_lossy()
+                .to_string(),
+            version: { unsafe { CStr::from_ptr(utsname.version.as_ptr()) } }
+                .to_string_lossy()
+                .to_string(),
+            machine: { unsafe { CStr::from_ptr(utsname.machine.as_ptr()) } }
+                .to_string_lossy()
+                .to_string(),
+            domainname: { unsafe { CStr::from_ptr(utsname.domainname.as_ptr()) } }
+                .to_string_lossy()
+                .to_string(),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -8,8 +71,20 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let operating_system = os.get(env::consts::OS);
 
+    let utsname = Utsname::new();
+
+    unsafe {
+        asm!(
+            "mov rax, 63",
+            "syscall",
+            in("rdi") &utsname as *const _,
+        );
+    }
+    let system_information = Uname::from(utsname);
+
+    println!("\t\t\t{}", system_information.nodename);
     println!(
-        "Операционная система: {} {}",
+        "Операционная система:\t{} {}",
         operating_system.expect("неизвестно операционная система"),
         env::consts::ARCH
     );
