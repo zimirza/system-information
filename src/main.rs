e9940cc095c21b7e877874dca4540134866bae70
zishan <zmirza@posteo.ch>
2024-05-16 17:22:48 +0200

    added tokio runtime and anyhow. use hashmap for operating system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::collections::HashMap;
 use std::env;
 
-fn main() {
-    println!("Операционная система: {} {}", env::consts::OS, env::consts::ARCH);
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut os = HashMap::new();
+    os.insert("linux", "Линукс");
+
+    let operating_system = os.get(env::consts::OS);
+
+    println!(
+        "Операционная система: {} {}",
+        operating_system.expect("неизвестно операционная система"),
+        env::consts::ARCH
+    );
+
+    Ok(())
 }
