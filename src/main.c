fdb1deba3744c10c600706de1252fd8640be3974
Зишан Мирза <149377404+zimirza@users.noreply.github.com>
2024-09-12 12:15:20 +0200

    add kernel

--- src/main.c
+++ src/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/utsname.h>
 #include <unistd.h>
 
-const char* get_hostname() {
+char* get_kernel() {
+    char* kernel = malloc(sizeof(char));
+    struct utsname buffer;
+
+    if (uname(&buffer) != 0) {
+        return "(no kernel)";
+    }
+
+    snprintf(kernel, sizeof(kernel) * 4, "%s %s\n", buffer.sysname, buffer.release);
+
+    return kernel;
+}
+
+char* get_hostname() {
     static char hostname[128];
     if (gethostname(hostname, 128) == 0) {
         hostname[128 - 1] = '\0';
@@ -11,8 +25,14 @@ const char* get_hostname() {
     return "(no hostname)";
 }
 
-int main() {
-    printf("%s@%s\n", getenv("USER"), get_hostname());
+int main(void) {
+    char* hostname = get_hostname();
+    char* kernel = get_kernel();
+
+    printf("%s@%s\n", getenv("USER"), hostname);
+    printf("%s\n", kernel);
+
+    free(kernel);
 
     return 0;
 }
